4c1da452e4b200770ba9e4a0f0917708fa20c693
MasterProjectLC <dan.inafuku@hotmail.com>
2021-06-26 17:47:47 -0300

    resolve income fix

--- commands/resolve_income.js
+++ commands/resolve_income.js
@@ -1,21 +1,26 @@
 var db = require('../external/database.js');
 
-var delta = {}
-
 const distribuirRecursos = (rows) => {
+    let delta = {};
+
     rows.forEach((row) => {
         if (!delta[row.nome])
             delta[row.nome] = [0, 0, 0];
 
-        switch (row.recurso) {
+        switch (row.recurso[0]) {
             case 'C':
                 delta[row.nome][1] += 1;
+                break;
             case 'R':
                 delta[row.nome][2] += 1;
+                break;
             default:
                 delta[row.nome][0] += parseInt(row.recurso);
+                break;
         }
     });
+
+    return delta;
 }
 
 // Exports
@@ -23,21 +28,22 @@ module.exports = {
     name: "resolve_income", 
     description: "resolve_income: dá recursos para todos os grupos baseado em seus territórios/presença.",
     execute: async (com_args, msg) => {
-        delta = {};
-        const promise1 = db.makeQuery(`SELECT * FROM grupos, terrestres 
+        const promise1 = db.makeQuery(`SELECT grupos.nome, recurso FROM grupos, terrestres 
         WHERE grupos.nome = terrestres.nação AND grupos.receita = 'Imposto'`).then((result) => {
-            distribuirRecursos(result.rows);
+            let delta = distribuirRecursos(result.rows);
 
             for (let nation in delta) {
-                db.makeQuery(`UPDATE jogadores SET recursos = recursos + $1 WHERE username = 
+                db.makeQuery(`UPDATE jogadores SET recursos = recursos + $1 WHERE cargo = 
                         (SELECT tesoureiro FROM grupos WHERE grupos.nome = $2)`, [delta[nation][0], nation]);
                 db.makeQuery(`UPDATE grupos SET commodities = $1 WHERE grupos.nome = $2`, [delta[nation][1], nation]);
                 db.makeQuery(`UPDATE grupos SET ruínas = $1 WHERE grupos.nome = $2`, [delta[nation][2], nation]);
             }
         });
 
-        const promise2 = db.makeQuery(`SELECT * FROM grupos, terrestres 
+        const promise2 = db.makeQuery(`SELECT grupos.nome, influência1, influência2 FROM grupos, terrestres 
         WHERE (grupos.nome = terrestres.influência1 OR grupos.nome = terrestres.influência2) AND grupos.receita = 'Influência'`).then((result) => {
+            let delta = {};
+
             result.rows.forEach((row) => {
                 if (!delta[row.nome])
                     delta[row.nome] = 0;
@@ -48,17 +54,17 @@ module.exports = {
             });
 
             for (let nation in delta) {
-                db.makeQuery(`UPDATE jogadores SET recursos = recursos + $1 WHERE username = 
+                db.makeQuery(`UPDATE jogadores SET recursos = recursos + $1 WHERE cargo = 
                         (SELECT tesoureiro FROM grupos WHERE grupos.nome = $2)`, [delta[nation], nation]);
             }
         });
 
-        const promise3 = db.makeQuery(`SELECT * FROM grupos, terrestres 
+        const promise3 = db.makeQuery(`SELECT grupos.nome, recurso FROM grupos, terrestres 
         WHERE (grupos.nome = terrestres.influência1 OR grupos.nome = terrestres.influência2) AND grupos.receita = 'Corporação'`).then((result) => {
-            distribuirRecursos(result.rows);
+            let delta = distribuirRecursos(result.rows);
 
             for (let nation in delta) {
-                db.makeQuery(`UPDATE jogadores SET recursos = recursos + $1 WHERE username = 
+                db.makeQuery(`UPDATE jogadores SET recursos = recursos + $1 WHERE cargo = 
                         (SELECT tesoureiro FROM grupos WHERE grupos.nome = $2)`, [delta[nation][0], nation]);
                 db.makeQuery(`UPDATE grupos SET commodities = $1 WHERE grupos.nome = $2`, [delta[nation][1], nation]);
                 db.makeQuery(`UPDATE grupos SET ruínas = $1 WHERE grupos.nome = $2`, [delta[nation][2], nation]);
