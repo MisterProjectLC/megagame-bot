0d8850d1cc0c560a8098bf46c807132b01284d36
MasterProjectLC <dan.inafuku@hotmail.com>
2021-06-27 20:13:50 -0300

    phase fix

--- index.js
+++ index.js
@@ -32,12 +32,10 @@ const get_channel = async (jogador_id) => {
 
 const get_phase = (guild) => {
     let member = guild.members.cache.find((member) => (member.user == Client.user));
-    console.log("Member ", member);
     if (member) {
         let role = member.roles.cache.find((role) => role.name.startsWith("Fase "));
-        console.log("Role ", role);
         if (role) {
-            console.log(role.name.slice(4));
+            console.log(parseInt(role.name.slice(4)));
             return parseInt(role.name.slice(4));
         }
     }
