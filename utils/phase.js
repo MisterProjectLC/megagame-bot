f271eddc876d591b618f1b489e532bc7ebf7ea9b
MasterProjectLC <dan.inafuku@hotmail.com>
2021-06-27 21:12:43 -0300

    phase fix

--- utils/phase.js
+++ utils/phase.js
@@ -1,8 +1,10 @@
 const get_phase = (guild) => {
     guild.members.cache.each((member) => {
         let role = member.roles.cache.find((role) => role.name.startsWith("Fase "));
-        if (role)
+        if (role) {
+            console.log(parseInt(role.name.slice(4)));
             return parseInt(role.name.slice(4));
+        }
     });
 
     return -1;
