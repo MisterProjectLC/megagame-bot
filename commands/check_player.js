5e0cc9f0c9e12d8a01c961c5dd55301ca3286a64
MasterProjectLC <dan.inafuku@hotmail.com>
2021-06-29 17:14:37 -0300

    check_player command

---
+++ commands/check_player.js
@@ -0,0 +1,24 @@
+var db = require('../external/database.js');
+
+// Exports
+module.exports = {
+    name: "check_player", 
+    description: "check_player <cargo>: checa o personagem com o cargo selecionado.",
+    execute: async (com_args, msg) => {
+        if (com_args < 1)
+        if (com_args.length < 1) {
+            msg.reply(args_invalidos);
+            return;
+        }
+
+        await db.getPlayer(com_args[0]).then((response) => {
+            let thisJogador = response.rows[0];
+            if (!thisJogador)
+                msg.reply("Esse personagem não existe.");
+            else {
+                msg.reply(thisJogador.username + ": " + thisJogador.time_nome + ", " + thisJogador.cargo + ". Recursos: " + thisJogador.recursos);
+            }
+        });
+    }, 
+    permission: (msg) => msg.member.roles.cache.some(role => role.name == "Moderador")
+};
