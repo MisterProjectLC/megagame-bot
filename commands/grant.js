8cf7a6276a92cbbb622761ba6d73a830136741db
MasterProjectLC <dan.inafuku@hotmail.com>
2021-07-01 08:10:05 -0300

    grant fix

--- commands/grant.js
+++ commands/grant.js
@@ -5,8 +5,8 @@ var log = require('./check_log.js');
 // Exports
 module.exports = {
     name: "grant", 
-    description: "grant <pessoa> <quantia>: aloca recursos para a pessoa.", 
-    execute: (com_args, msg) => {
+    description: "grant <cargo> <quantia>: aloca recursos para a pessoa com o cargo especificado.", 
+    execute: async (com_args, msg) => {
         // Check args
         if (com_args.length < 2) {
             msg.reply(args_invalidos);
@@ -23,11 +23,10 @@ module.exports = {
             }
         }
         
-        log.logCommand(msg, "aloca " + dinheiro + " recursos para " + com_args[0] + ".", "grant", com_args, dinheiro);
-    }, 
-    permission: (msg, phase) => msg.member.roles.cache.some(role => role.name == "Chefe de Estado") && phase == 0,
-    command: (com_args) => {
         db.makeQuery(`UPDATE jogadores SET recursos = recursos + $1 WHERE cargo = $2;`,
-        [dinheiro, com_args[0]]).then(() => msg.reply("Pago."));
-    }
+        [dinheiro, com_args[0]]).then((res) => {
+            console.log(res);
+        });
+    }, 
+    permission: (msg, phase) => msg.member.roles.cache.some(role => role.name == "Chefe de Estado") && phase == 0
 };
