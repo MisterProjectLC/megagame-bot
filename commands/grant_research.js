af0c47d2c4e3bec83b7a91218cdf8d4186f626fa
MasterProjectLC <dan.inafuku@hotmail.com>
2021-07-03 08:46:29 -0300

    grant research fix

--- commands/grant_research.js
+++ commands/grant_research.js
@@ -5,7 +5,7 @@ var areas = require('../data/research_areas.json').areas;
 // Exports
 module.exports = {
     name: "grant_research", 
-    description: "grant_research <especialização> <área> <publicador> <nação>: dá especialização do publicador para a nação.", 
+    description: "grant_research <especializações> <área> <publicador> <nação>: dá especialização do publicador para a nação.", 
     min: 4, max: 4,
     execute: async (com_args, msg) => {
         // Check args
@@ -14,12 +14,13 @@ module.exports = {
             return;
         }
 
-        await db.makeQuery(`INSERT INTO pesquisas VALUES ($1, $2, $3)`,
-            [com_args[0], com_args[1], com_args[3]]).then(() => {
-                db.makeQuery(`UPDATE opiniões SET valor = valor + 1 WHERE sujeito = $2 AND objeto = $1`,
+        let esps = com_args[1].split(', ');
+        esps.forEach((esp) => {
+            db.makeQuery(`INSERT INTO pesquisas VALUES ($1, $2, $3)`, [com_args[0], esp, com_args[3]]);
+        })
+
+        db.makeQuery(`UPDATE opiniões SET valor = valor + 1 WHERE sujeito = $2 AND objeto = $1`,
                         [com_args[2], com_args[3]]).then(() => msg.reply("Dado."), () => msg.reply(args_invalidos));
-            }, 
-        () => msg.reply(args_invalidos));
     }, 
     permission: (msg) => msg.member.roles.cache.some(role => role.name == "Moderador")
 };
