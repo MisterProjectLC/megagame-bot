a3ef99b6dc649fec504ce3d2e86530dcf5f1b55b
MasterProjectLC <dan.inafuku@hotmail.com>
2021-07-04 21:05:27 -0300

    log fix

--- commands/check_log.js
+++ commands/check_log.js
@@ -123,8 +123,8 @@ module.exports = {
         if (response.length == 0)
             msg.reply("log vazio.");
         else
-            for (let i = 0; i < response.length; i += 20)
-                msg.reply(response.slice(i, i+20).join("\n"));
+            for (let i = 0; i < response.length; i += 15)
+                msg.reply(response.slice(i, i+15).join("\n"));
     }, 
     permission: (msg) => msg.member.roles.cache.some(role => role.name == "Participante" || role.name == "Espectador"),
 
