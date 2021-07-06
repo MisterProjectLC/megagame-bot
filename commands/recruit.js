a84527f45dcd3b21b65582f3f340dcb9219f2b16
MasterProjectLC <dan.inafuku@hotmail.com>
2021-07-06 08:35:35 -0300

    execute log fix

--- commands/recruit.js
+++ commands/recruit.js
@@ -32,11 +32,11 @@ module.exports = {
                         "recruit", com_args, tropas);
     }, 
     permission: (msg, phase) => msg.member.roles.cache.some(role => role.name == "Militar") && phase == 1,
-    command: (com_args, msg) => {
+    command: (com_args, author_id) => {
         if (com_args[2])
             db.makeQuery(`UPDATE terrestres SET tropas = tropas + $1 WHERE nome = $2`, [com_args[1], com_args[0]]);
         else
             db.makeQuery(`INSERT INTO frotas VALUES ($1, (SELECT time_nome FROM jogadores WHERE jogador_id = $2), $3)`, 
-                        [com_args[0], msg.author.id, tropas]);
+                        [com_args[0], author_id, tropas]);
     }
 };
