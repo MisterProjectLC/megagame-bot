5c996e03f38abce3b975d4a99fb8a3b2aa9a0681
MasterProjectLC <dan.inafuku@hotmail.com>
2021-07-07 08:50:28 -0300

    check_nation_mod

--- commands/check_nation_mod.js
+++ commands/check_nation_mod.js
@@ -6,7 +6,7 @@ module.exports = {
     description: "check_nation_mod <nação>: mostra a Lealdade e Opiniões Públicas da nação escolhida.",  
     min: 1, max: 1,
     execute: async (com_args, msg) => {
-        await db.makeQuery(`SELECT lealdade, objeto, valor, nome
+        await db.makeQuery(`SELECT lealdade, objeto, valor, nações.nome
         FROM nações, opiniões WHERE nações.nome = $1 AND nações.nome = opiniões.sujeito ORDER BY valor`, [com_args[0]]).then((result) => {
             if (!result.rows[0])
                 return;
