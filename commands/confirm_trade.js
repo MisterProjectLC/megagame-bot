94489bc3fe0ce04d4e6251466f0978307f1defa8
MasterProjectLC <dan.inafuku@hotmail.com>
2021-07-08 20:38:38 -0300

    fix de check log

--- commands/confirm_trade.js
+++ commands/confirm_trade.js
@@ -8,8 +8,8 @@ module.exports = {
     description: "confirm_trade <grupo>: confirma a oferta do grupo escolhido. AVISO: Depois de confirmada, você NÃO pode cancelá-la!",  
     min: 2, max: 2,
     execute: async (com_args, msg, send_message) => {
-        await db.makeQuery(`UPDATE trocas SET confirmado = true WHERE ofertante = (SELECT time_nome FROM jogadores WHERE jogador_id = $1) AND
-        ofertado = $2`, [msg.author.id, com_args[0]]).then(async (response) => {
+        await db.makeQuery(`UPDATE trocas SET confirmado = true WHERE ofertante = (SELECT time_nome FROM jogadores WHERE jogador_id = $2) AND
+        ofertado = $1`, [msg.author.id, com_args[0]]).then(async (response) => {
             if (response.rowCount <= 0) {
                 msg.reply(args_invalidos);
                 return;
