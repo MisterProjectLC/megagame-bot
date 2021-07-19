5cdef4d6226442622f391344c99a59364c880208
MasterProjectLC <dan.inafuku@hotmail.com>
2021-07-19 08:57:53 -0300

    normalize opinions

--- commands/normalize_opinions.js
+++ commands/normalize_opinions.js
@@ -6,11 +6,12 @@ var log = require('./check_log.js');
 module.exports = {
     name: "normalize_opinions", 
     description: `normalize_opinions: faz alterações nas opiniões e nas lealdades para normalizá-las.`, 
-    min: 1, max: 3,
+    min: 0, max: 0,
     execute: async (com_args, msg) => {
         db.makeQuery(`UPDATE opiniões SET valor = valor - 1 WHERE valor > 2`);
         db.makeQuery(`UPDATE opiniões SET valor = valor + 1 WHERE valor < -3`);
         db.makeQuery(`UPDATE nações SET lealdade = lealdade - GREATEST(0, (lealdade-15)/5)`);
+        msg.reply("Feito.")
     }, 
     permission: (msg) => msg.member.roles.cache.some(role => role.name == "Moderador")
 };
