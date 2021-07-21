c1e6a81c8573d8b4d5256b9963574c897dae6d16
MasterProjectLC <dan.inafuku@hotmail.com>
2021-07-21 11:23:15 -0300

    custo do custom adicionado

--- commands/custom.js
+++ commands/custom.js
@@ -5,14 +5,19 @@ var log = require('./check_log.js');
 // Exports
 module.exports = {
     name: "custom", 
-    description: "custom <ordem>: faz um comando especial. Serve para ordens que não encaixam em nenhum outro comando", 
-    min: 1, max: 1,
+    description: "custom <ordem> [custo]: faz um comando especial. Serve para ordens que não encaixam em nenhum outro comando. Se custo não for dado, assume 1.", 
+    min: 1, max: 2,
     execute: (com_args, msg) => {
-        if (com_args.length < 1) {
-            msg.reply(args_invalidos);
-            return;
+        let custo = 1;
+        if (com_args.length >= 2) {
+            let i = parseInt(com_args[1]);
+            if  (i !== i) {
+                msg.reply(args_invalidos);
+                return;
+            } else
+                custo = i;
         }
-        log.logCommand(msg, com_args[0], "custom", com_args);
+        log.logCommand(msg, com_args[0], "custom", com_args, custo);
     }, 
     permission: (msg) => msg.member.roles.cache.some(role => role.name == "Participante" || role.name == "Espectador"),
     command: (com_args) => {
