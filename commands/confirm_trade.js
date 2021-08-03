5bb6e8144976b6595708bb4dcfedf497d1f9c38e
MasterProjectLC <dan.inafuku@hotmail.com>
2021-08-03 20:34:39 -0300

    turing testers now can use confirm_trade

--- commands/confirm_trade.js
+++ commands/confirm_trade.js
@@ -8,7 +8,7 @@ module.exports = {
     description: "confirm_trade <grupo>: confirma a oferta do grupo escolhido.",  
     min: 1, max: 1,
     execute: async (com_args, msg, send_message) => {
-        await db.makeQuery(`SELECT * FROM trocas WHERE ofertado = (SELECT grupos.nome FROM grupos, jogadores 
+        await db.makeQuery(`SELECT * FROM trocas WHERE ofertado = ANY (SELECT grupos.nome FROM grupos, jogadores 
         WHERE grupos.tesoureiro = jogadores.cargo AND jogador_id = $1) AND ofertante ILIKE $2`, [msg.author.id, com_args[0]]).then(async (response) => {
             if (response.rows.length <= 0) {
                 msg.reply(args_invalidos);
